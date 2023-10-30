98091e0650d74c67794b750977f00912098aec65
pongchul <inchul7511@gmail.com>
2023-10-30 18:13:38 +0900

    Featuring : Revise Domain name

---
+++ backend/src/main/java/com/SLACK/backend/member/domain/Email.java
@@ -0,0 +1,45 @@
+package com.SLACK.backend.member.domain;
+
+import com.SLACK.backend.member.exception.MemberException;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static com.SLACK.backend.member.exception.MemberErrorCode.*;
+
+@Getter
+@Embeddable
+@NoArgsConstructor
+public class Email {
+
+    private static final int MIN_LENGTH = 8;
+    private static final int MAX_LENGTH = 30;
+    private static final String EMAIL_FORMAT = "@";
+
+    @Column(name = "email", unique = true)
+    private String value;
+
+    public Email(String value) {
+        this.value = value;
+    }
+
+    public static Email from(String value) {
+        validLengthInRange(value);
+        validEmailFormat(value);
+        return new Email(value);
+    }
+
+    public static void validLengthInRange(String value) {
+        int length = value.length();
+        if (length > MAX_LENGTH || length < MIN_LENGTH) {
+            throw new MemberException(EMAIL_IS_WRONG_LENGTH);
+        }
+    }
+
+    public static void validEmailFormat(String value) {
+        if (!value.contains(EMAIL_FORMAT)) {
+            throw new MemberException(EMAIL_IS_WRONG_FORMAT);
+        }
+    }
+}
