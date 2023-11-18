4f8c444b6dc78a4dade095473c70b06987624bfe
pongchul <inchul7511@gmail.com>
2023-11-18 14:05:43 +0900

    Featuring : Added Channel Error Code

--- backend/src/main/java/com/SLACK/backend/channel/domain/Channel.java
+++ backend/src/main/java/com/SLACK/backend/channel/domain/Channel.java
@@ -1,33 +1,60 @@
 package com.SLACK.backend.channel.domain;
 
 import com.SLACK.backend.common.BaseTimeEntity;
-import com.SLACK.backend.member.domain.Member;
 import com.SLACK.backend.workspace.domain.Workspace;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "tb_channel")
 public class Channel extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "owner_id")
-    private Member host;
-
     @Embedded
     private ChannelName name;
 
     @Column(name = "private")
-    private int privateNm;
+    private boolean isPrivate;  // 비공개 여부
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "Workspace_id")
-    private Workspace workspace;
+    @JoinColumn(name = "workspace_id")
+    private Workspace workspaceId;
+
+    public Channel(Long id, ChannelName name, boolean isPrivate, Workspace workspaceId) {
+        this.id = id;
+        this.name = name;
+        this.isPrivate = isPrivate;
+        this.workspaceId = workspaceId;
+    }
+
+    public Channel(Long id, String name, boolean isPrivate, Workspace workspaceId) {
+        this.id = id;
+        this.name = ChannelName.from(name);
+        this.isPrivate = isPrivate;
+        this.workspaceId = workspaceId;
+    }
+
+    public String getName() {
+        return name.getValue();
+    }
+
+    public List<Channel> getChannels() {
+        List<Channel> channels = new ArrayList<>();
+        channels.stream()
+                .map(Channel::getChannels)
+                .collect(Collectors.toUnmodifiableList());
+
+        return channels;
+    }
 }
 
--- backend/src/main/java/com/SLACK/backend/channel/exception/ChannelErrorCode.java
+++ backend/src/main/java/com/SLACK/backend/channel/exception/ChannelErrorCode.java
@@ -4,8 +4,10 @@ import com.SLACK.backend.global.exception.ErrorCode;
 
 public enum ChannelErrorCode implements ErrorCode {
 
-    CHANNEL_NAME_IS_NOT_BLANK(400,"CHANNEL_001","워크스페이스 이름은 공백이 될 수 없습니다."),
-    CHANNEL_NAME_CANNOT_BE_OUT_OF_RANGE(400, "CHANNEL_002","워크스페이스 이름은 1자 이상 20자 이하 여야 합니다."),
+
+    CHANNEL_IS_NOT_EXIST(404, "CHANNEL_000", "채널이 존재하지 않습니다."),
+    CHANNEL_NAME_IS_NOT_BLANK(400,"CHANNEL_001","채널 이름은 공백이 될 수 없습니다."),
+    CHANNEL_NAME_CANNOT_BE_OUT_OF_RANGE(400, "CHANNEL_002","채널 이름은 1자 이상 20자 이하 여야 합니다."),
     CHANNEL_CHAT_IS_OUT_OF_RANGE(400, "CHANNEL_003", "메시지는 255자 이하여야 합니다."),
 
 
