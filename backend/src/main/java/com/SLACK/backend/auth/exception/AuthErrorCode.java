e07c535b02b7b08cc1e799a6886e3890fb5533db
pongchul <inchul7511@gmail.com>
2023-12-02 21:15:17 +0900

    Featuring : Added error code

--- backend/src/main/java/com/SLACK/backend/auth/exception/AuthErrorCode.java
+++ backend/src/main/java/com/SLACK/backend/auth/exception/AuthErrorCode.java
@@ -4,7 +4,13 @@ import com.SLACK.backend.common.exception.ErrorCode;
 
 public enum AuthErrorCode implements ErrorCode {
 
-    NOT_FOUND(400,"AUTH_001","토큰이 존재하지 않습니다.");
+    NOT_FOUND(400,"AUTH_001","토큰이 존재하지 않습니다."),
+    NEED_TOKEN(404,"AUTH_002","토큰이 필요합니다."),
+    INVALID_TOKEN(400, "AUTH_003", "잘못된 토큰입니다."),
+    TOKEN_IS_EXPIRED(404, "AUTH_004","토큰이 만료됐습니다."),
+
+
+    ;
 
     private final int statusCode;
     private final String errorCode;
