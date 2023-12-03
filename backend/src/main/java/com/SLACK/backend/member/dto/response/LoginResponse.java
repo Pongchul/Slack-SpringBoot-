20333f28f5d2eb4fc28876dab84dc95d66e43cae
pongchul <inchul7511@gmail.com>
2023-12-03 17:44:23 +0900

    Refactoring : Change from an existing class to a record.

--- backend/src/main/java/com/SLACK/backend/member/dto/response/LoginResponse.java
+++ backend/src/main/java/com/SLACK/backend/member/dto/response/LoginResponse.java
@@ -1,29 +1,17 @@
 package com.SLACK.backend.member.dto.response;
 
+import com.SLACK.backend.auth.dto.TokenDto;
 import com.SLACK.backend.member.domain.Member;
-import com.SLACK.backend.workspace.domain.Workspace;
-import com.SLACK.backend.workspace.dto.response.WorkspaceResponse;
-import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-public class LoginResponse {
+public record LoginResponse (
 
-    @NotNull
-    private Long id;
+        String accessToken,
+        String refreshToken,
+        MemberResponse memberResponse
 
-    @NotNull
-    private String email;
+) {
 
-    @NotNull
-    private String nickname;
-
-    public static LoginResponse toResponse(Member member) {
-        return new LoginResponse(member.getId(), member.getEmail(), member.getNickname());
+    public static LoginResponse of(TokenDto tokenDto, Member member) {
+        return new LoginResponse(tokenDto.accessToken(), tokenDto.refreshToken(), MemberResponse.toResponse(member));
     }
 }
