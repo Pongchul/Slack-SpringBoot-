ba2124c1bb73c8e030ea10db1fa195a41e69b8aa
pongchul <inchul7511@gmail.com>
2023-12-09 13:57:32 +0900

    Featuring : jwt logic !

--- backend/src/main/java/com/SLACK/backend/member/dto/MemberTokenDto.java
+++ backend/src/main/java/com/SLACK/backend/member/dto/MemberTokenDto.java
@@ -2,12 +2,7 @@ package com.SLACK.backend.member.dto;
 
 import com.SLACK.backend.member.domain.Member;
 
-public record MemberTokenDto(
-        Long id,
-        String email,
-        String nickName,
-        String token
-) {
+public record MemberTokenDto(Long id, String email, String nickName, String token) {
 
     public static MemberTokenDto from(Member member, String token) {
         return new MemberTokenDto(member.getId(), member.getEmail(), member.getNickname(), token);
