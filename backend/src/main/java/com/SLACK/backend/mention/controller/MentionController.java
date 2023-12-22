642392d0f4f24c4cf3e0afef77036a1a9038d7a9
pongchul <inchul7511@gmail.com>
2023-12-22 20:33:11 +0900

    Refactoring : Revised URL

--- backend/src/main/java/com/SLACK/backend/mention/controller/MentionController.java
+++ backend/src/main/java/com/SLACK/backend/mention/controller/MentionController.java
@@ -1,7 +1,11 @@
 package com.SLACK.backend.mention.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping("/api/mentions")
 public class MentionController {
+
 }
