bf655b9cd89e034d6e7cc67ce54f79481cec69b0
pongchul <inchul7511@gmail.com>
2023-12-25 10:57:21 +0900

    Refactoring : Added lombok annotation

--- backend/src/main/java/com/SLACK/backend/mention/controller/MentionController.java
+++ backend/src/main/java/com/SLACK/backend/mention/controller/MentionController.java
@@ -1,11 +1,13 @@
 package com.SLACK.backend.mention.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/api/mentions")
+@RequiredArgsConstructor
 public class MentionController {
 
 }
