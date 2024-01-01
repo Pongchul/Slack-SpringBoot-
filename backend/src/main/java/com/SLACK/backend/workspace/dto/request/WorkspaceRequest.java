6e38740ede57b306a5cf291d3d9fbc5ad9db62b6
pongchul <inchul7511@gmail.com>
2024-01-01 18:02:59 +0900

    Refactoring : Added AccessLevel !

--- backend/src/main/java/com/SLACK/backend/workspace/dto/request/WorkspaceRequest.java
+++ backend/src/main/java/com/SLACK/backend/workspace/dto/request/WorkspaceRequest.java
@@ -1,12 +1,13 @@
 package com.SLACK.backend.workspace.dto.request;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class WorkspaceRequest {
 
